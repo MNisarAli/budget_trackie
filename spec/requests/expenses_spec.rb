a9db0bfe7325fcdb1ca18f576c4762d42c32efff
MNisarAli <dr.nisaralig@gmail.com>
2024-02-06 11:06:00 +0500

    Add request specs for expenses controller

--- spec/requests/expenses_spec.rb
+++ spec/requests/expenses_spec.rb
@@ -1,7 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe 'Expenses', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  before(:example) do
+    @user = User.create(name: 'Test User', email: 'test@gmail.com', password: 'password')
+
+    get new_user_session_path
+    post user_session_path, params: { user: { email: @user.email, password: @user.password } }
+
+    @category = Category.create(name: 'Food', icon: 'https://img.icons8.com/ios/50/000000/food.png', user: @user)
+    @expense = Expense.create(name: 'Pizza', amount: 10, author: @user, category_ids: [@category.id])
+  end
+
+  describe 'GET /new' do
+    before do
+      get new_user_category_expense_path(@user, @category)
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the new template' do
+      expect(response).to render_template(:new)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      expect(response.body).to include('New Expense')
+    end
+  end
+
+  describe 'POST /create' do
+    before do
+      post user_category_expenses_path(@user, @category),
+           params: { expense: { name: 'Pizza', amount: 10, author: @user, category_ids: [@category.id] } }
+    end
+
+    it 'redirects to the index page' do
+      expect(response).to redirect_to(user_category_path(@user, @category))
+    end
+
+    it 'includes the correct flash message' do
+      follow_redirect!
+      expect(response.body).to include('Expense added successfully')
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    before do
+      delete user_category_expense_path(@user, @category, @expense)
+    end
+
+    it 'redirects to the categories page' do
+      expect(response).to redirect_to(user_categories_path(@user))
+    end
+
+    it 'includes the correct flash message' do
+      follow_redirect!
+      expect(response.body).to include('Expense has been successfully removed')
+    end
   end
 end
