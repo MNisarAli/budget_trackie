6ff951d853afe203527469e4656778b4bb6b0709
MNisarAli <dr.nisaralig@gmail.com>
2024-02-06 10:02:17 +0500

    Update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_05_103718) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_05_123924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_expenses", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "expense_id", null: false
+  end
+
+  create_table "expenses", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.string "name", null: false
+    t.decimal "amount", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_expenses_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -27,4 +50,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_05_103718) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "expenses", "users", column: "author_id"
 end
