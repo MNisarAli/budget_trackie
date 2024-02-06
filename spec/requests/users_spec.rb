49100a48055b8af5933318c235dd5e2842741a3e
MNisarAli <dr.nisaralig@gmail.com>
2024-02-06 10:57:23 +0500

    Add request specs for users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) { get users_path }
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      expect(response.body).to include('Budget Trackie')
+    end
   end
 end
